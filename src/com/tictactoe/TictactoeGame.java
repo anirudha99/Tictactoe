a7db53680c48b2c925cbca54d6f38fcbaa03ba30
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-26 11:07:32 +0530

    Added use case 1:Creating board

--- src/com/tictactoe/TictactoeGame.java
+++ src/com/tictactoe/TictactoeGame.java
@@ -1,8 +1,19 @@
 package com.tictactoe;
 
 public class TictactoeGame {
+	
+	char[] board = new char[10];
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe game");
+		TictactoeGame tictactoe = new TictactoeGame();
+		tictactoe.createBoard();
+	}
+	
+	public void createBoard() {
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
 	}
 
 }
